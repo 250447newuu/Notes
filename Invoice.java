22d9f3fee884e2c3c4465a9f9488e1c6a8054237
Asrorbek <250447@newuu.uz>
2026-02-21 16:38:50 +0500

    Add files via upload

--- Invoice.java
+++ Invoice.java
@@ -1,6 +1,50 @@
 public class Invoice {
-    private String number;
-    private String description;
+    private String partNumber;
+    private String partDescription;
     private int quantity;
-    private double
+    private double pricePerItem;
+    Invoice(){
+        this("CC0","CocaCola0",4,0.85);
+    }
+    public Invoice(String partNumber,String partDescription, int quantity, double pricePerItem){
+        setQuantity(quantity);
+        setPricePerItem(pricePerItem);
+        setPartNumber(partNumber);
+        setPartDescription(partDescription);
+    }
+    public void setPartNumber(String partNumber){
+        this.partNumber=partNumber;
+    }
+    public void setPartDescription(String partDescription){
+        this.partDescription=partDescription;
+    }
+    public void setQuantity(int quantity){
+        if(quantity<0){
+            this.quantity=0;
+        }else{
+            this.quantity=quantity;
+        }
+    }
+    public void setPricePerItem(double pricePerItem){
+        if(pricePerItem<0){
+            this.pricePerItem=0.0;
+        }else{
+            this.pricePerItem=pricePerItem;
+        }
+        }
+        public String getPartNumber(){
+        return partNumber;
+        }
+        public String getPartDescription(){
+        return partDescription;
+        }
+        public int getQuantity(){
+        return quantity;
+        }
+        public double getPricePerItem(){
+        return  pricePerItem;
+        }
+        public double calculateInvoice(){
+        return pricePerItem*quantity;
+        }
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.problem1();
+        m.problem2();
+    }
     void problem1() {
         Rectangle r1 = new Rectangle();
         Rectangle r2 = new Rectangle(5, 10);
@@ -14,9 +19,21 @@ public class Main {
         System.out.println("The perimeter:" + r2.calculatePerimeter());
         System.out.println("----------------------------------------");
     }
-    public static void main(String[] args) {
-        Main m = new Main();
-        m.problem1();
-        m.problem2();
-        }
+    void problem2(){
+        Invoice Inv1= new Invoice();
+        Invoice Inv2= new Invoice("H123","Hammer",5,15.99);
+        System.out.println("----------------------------------------");
+        System.out.println("Part Number:" + Inv1.getPartNumber());
+        System.out.println("Description:" + Inv1.getPartDescription());
+        System.out.println("Quantity:" + Inv1.getQuantity());
+        System.out.println("Price Per Item:" + Inv1.getPricePerItem());
+        System.out.println("Invoice:" + Inv1.calculateInvoice());
+        System.out.println("----------------------------------------");
+        System.out.println("Part Number:" + Inv2.getPartNumber());
+        System.out.println("Description:" + Inv2.getPartDescription());
+        System.out.println("Quantity:" + Inv2.getQuantity());
+        System.out.println("Price Per Item:" + Inv2.getPricePerItem());
+        System.out.println("Invoice:" + Inv2.calculateInvoice());
+        System.out.println("----------------------------------------");
+    }
 }
